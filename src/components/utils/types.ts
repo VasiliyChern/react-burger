c4d2199b3e0c7f83c20a9812414c5e54d72d4608
vchernousov@npkinform.ru <vchernousov@npkinform.ru>
2023-01-11 23:28:16 +0300

    заменил types.js на types.ts

---
+++ src/components/utils/types.ts
@@ -0,0 +1,15 @@
+export type ingredientType = {
+    readonly "_id": string,
+    readonly "name": string,
+    readonly "type": string,
+    readonly "proteins": number,
+    readonly "fat": number,
+    readonly "carbohydrates": number,
+    readonly "calories": number,
+    readonly "price": number,
+    readonly "image": string,
+    readonly "image_mobile": string,
+    readonly "image_large": string,
+    readonly "__v": 0,
+    key?: string
+}
